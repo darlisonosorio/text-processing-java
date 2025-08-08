2dbd751db631aab7be5b0063ccf7d3dd9489d689
Darlison Osorio <darlison.osorio@alpinedatasolutions.com>
2025-08-08 16:11:57 -0400

    feat: Implemented the anagram function and a main function for test

--- textprocessing/src/Main.java
+++ textprocessing/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Arrays.stream(args)
+                .forEach(val -> {
+                    final List<String> anagrams = AnagramsUtil.generateAnagrams(val);
+                    System.out.println(anagrams.size() + " anagramas encontrados para " + val + " :");
+                    System.out.println("[ " + String.join(", ", anagrams) + " ]");
+                });
+
     }
 }
